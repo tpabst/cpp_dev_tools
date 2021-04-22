fadeac4c82f21ca07b99dfa73e94caa0e176848e
Thomas Pabst <thomas.pabst@gmail.com>
2021-04-22 15:25:37 +0200

    Fix external KDESyntaxHighlighting due to the evolving of master

--- ExternalKDESyntaxHighlighting.cmake
+++ ExternalKDESyntaxHighlighting.cmake
@@ -4,7 +4,7 @@ ExternalProject_Add(KDESyntaxHighlighting
   DEPENDS ExtraCmakeModules
   #--Download step --------------
   GIT_REPOSITORY ${CDT_KDE_SYNTAX_HIGHLIGHTING}
-  GIT_TAG master
+  GIT_TAG v5.30.0-rc1-961-ge5f74048
   TIMEOUT 30
   #--Configure step-------------
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
